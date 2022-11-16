53f57bbb02b9cc2bfb9532b13c9313f0f0bfa777
Kacper Chmielewski <kchmielewski@tlen.pl>
2022-11-16 14:51:34 +0100

    Yelp-MVP --> models, controller, views created

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 20.times do
   Restaurant.create(
-    name:Faker::Restaurant.name,
+    name: Faker::Restaurant.name,
     address:Faker::Address.street_address,
     phone_number:Faker::PhoneNumber.phone_number,
-    category:Faker::Restaurant.type
+    category: "chinese"
   )
 end
 
